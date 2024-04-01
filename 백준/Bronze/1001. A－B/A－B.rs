a87bf96a1116badcbe4dac7baadeaeae09ad189e
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-04-01 21:27:34 +0900

    [Bronze V] Title: A-B, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.rs
@@ -0,0 +1,13 @@
+use std::io::{stdin,stdout,Write,BufWriter};
+
+fn main() {
+  let mut input = String::new();
+  stdin().read_line(&mut input).unwrap();
+  let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
+
+
+  let a = input.next().unwrap();
+  let b = input.next().unwrap();
+
+  println!("{}",a-b);
+}
