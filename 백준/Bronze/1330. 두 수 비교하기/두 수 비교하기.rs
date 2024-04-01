ed91fdf5eccf2da0207695cbad15a54fa0b92aad
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-04-01 21:51:19 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.rs
@@ -0,0 +1,23 @@
+use std::io::{stdin,stdout,Write,BufWriter};
+
+fn main() {
+  let stdout = stdout();
+  let mut out = BufWriter::new(stdout.lock());
+
+  let mut input = String::new();
+  stdin().read_line(&mut input).unwrap();
+  let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
+
+  let a = input.next().unwrap();
+  let b = input.next().unwrap();
+
+  if a>b{
+    writeln!(out,">");
+  }
+  else if a==b {
+    writeln!(out,"==");
+  }
+  else {
+    writeln!(out,"<");
+  }
+}
