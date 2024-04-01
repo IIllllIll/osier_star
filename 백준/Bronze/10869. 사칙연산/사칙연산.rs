aa48ad5ee32f85dc43938ab3dbe573ce3db543f4
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-04-01 21:39:59 +0900

    [Bronze V] Title: 사칙연산, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.rs
@@ -0,0 +1,18 @@
+use std::io::{stdin,stdout,Write,BufWriter};
+
+fn main() {
+  let stdout = stdout();
+  let mut out = BufWriter::new(stdout.lock());
+  
+  let mut input = String::new();
+  stdin().read_line(&mut input).unwrap();
+  let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
+
+  let a = input.next().unwrap();
+  let b = input.next().unwrap();
+  writeln!(out,"{}",a+b);
+  writeln!(out,"{}",a-b);
+  writeln!(out,"{}",a*b);
+  writeln!(out,"{}",a/b);
+  writeln!(out,"{}",a%b);
+}
