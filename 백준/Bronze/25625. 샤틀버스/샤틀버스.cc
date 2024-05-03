822ecf9323338b48912dcd31db06871a0791786a
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-05-03 12:16:21 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define point pair<long long, long long>
+#define x first
+#define y second
+
+
+
+void solve(){
+  int a,b;
+  cin>>a>>b;
+  if(a>=b) cout<<a+b;
+  else cout<<b-a;
+  return;
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int ct=1;
+  //cin>>ct;
+  while(ct--) solve();
+  return 0;
+}
