dbad3c6a1798fe5470d3073c7fd55471ac6d64ba
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-05-07 17:08:14 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define point pair<long long, long long>
+#define x first
+#define y second
+
+void solve(){
+  int n;
+  char a;
+  cin>>n>>a;
+  while(n--) cout<<a;
+  cout<<'\n';
+  return;
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int ct=1;
+  cin>>ct;
+  while(ct--) solve();
+  return 0;
+}
