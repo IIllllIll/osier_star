d4df93f92cf6bdc7d37afe43c795714d514fcb55
osier_star <89204576+IIllllIll@users.noreply.github.com>
2024-06-16 00:46:01 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define point pair<long long, long long>
+#define x first
+#define y second
+
+void solve(){
+  long long n;
+  cin>>n;
+  cout<<fixed;
+  cout.precision(8);
+  cout<<sqrt(n)*4;
+  return;
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int ct=1;
+  //cin>>ct;
+  while(ct--) solve();
+  return 0;
+}
